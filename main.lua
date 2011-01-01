8fce125aeba1e1c0c089ac5e48224236f6bd78af
Andy A <5thWall@gmail.com>
2010-12-31 20:13:32 -0700

    Draw ship at proper offset

--- main.lua
+++ main.lua
@@ -69,7 +69,7 @@ function Ship:past_max( )
 end
 
 function Ship:render( )
-	gfx.draw(self.image, self.x_pos, self.y_pos, self.angle, 0.5, 0.5, 16, 16)
+	gfx.draw(self.image, self.x_pos, self.y_pos, self.angle, 0.5, 0.5, 32, 32)
 end
 
 -- Starfield
